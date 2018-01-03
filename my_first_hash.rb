67520ad65a56591640110c551838826f57a9e152
ANNA YANG <yanganna68@gmail.com>
2018-01-03 22:37:10 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,11 +1,12 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  your_hash = {"firstname" => "Anna", "lastname" => "Yang"}
   
 end
 
 
 def shipping_manifest
+  shipping_manifest = {"bone corsets" => 2, "porcelain vases" => 2, "oil paintings" => 3}
   #set a variable called `shipping_manifest`, equal to a hash
   #fill that hash with key/value pairs that describe the following information: 
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
